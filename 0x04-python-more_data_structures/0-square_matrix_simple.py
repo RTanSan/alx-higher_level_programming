9f63737f52f321ed7e0ef45dde1051b9bf6da7f0
RTanSan <quasartan030@gmail.com>
2024-03-20 15:28:52 -0700

    computes the square value of all integers of a matrix.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    for n in matrix:
+        return n
+    for m in n:
+        m ** 2
+        return m
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,5 @@
+Data Structures: Set, Dictionary
+Tasks
+
+0. Computes the square value of all integers of a matrix.
+Prototype: def square_matrix_simple(matrix=[]):
